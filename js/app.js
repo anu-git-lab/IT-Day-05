76f0856e89fbe634d0ccab695036cbb5ace54650
anu-git-lab <rejinoldanu23@gmail.com>
2026-06-27 14:40:20 +0530

    Comment out initial variable declarations and console logs in app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,50 @@
 
 // console.log("Hello Java Script");
 
-let name = "Anu";
+// let name = "Anu";
 
-let age = 18;
+// let age = 18;
 
-name = "ranil";
+// name = "ranil";
 
-console.log(name);
-console.log(age);
+// console.log(name);
+// console.log(age);
 
  
-alert("Hello Icet");
+// alert("Hello Icet");
 
 //Sting name = "Anu";
+
+// let name = "saman";
+// let age = 18;
+// let isAfterAL = false;
+// let customer = [];
+// let customer = {};
+// let salary = 7500.0;
+
+// console.log(typeof name);
+// console.log(typeof customer);
+
+// --------------------------------------
+
+// + | - | * | /
+// let x = 10;
+// let y = 20;
+
+// let z = x / y;
+
+// console.log(z);
+
+// -----------------------------------
+
+// > | < | >= | <= | == | !=
+// >== | <== | === | !==
+
+let x = 10;
+
+let y = "10";
+
+console.log(x===y);
+
+
+
