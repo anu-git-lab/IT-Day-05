b0c922f5c61d54185a69fb8d506e07d2955122a3
anu-git-lab <rejinoldanu23@gmail.com>
2026-06-27 13:00:22 +0530

    Refactor index.html and add app.js and app.ts files

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+// console.log("Hello Java Script");
+
+let name = "Anu";
+
+let age = 18;
+
+name = "ranil";
+
+console.log(name);
+console.log(age);
+
+ 
+alert("Hello Icet");
+
+//Sting name = "Anu";
